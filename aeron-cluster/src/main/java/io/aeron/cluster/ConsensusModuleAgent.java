14ba8ea4e9e529424277a5943904752810c54dd8
marc-adaptive <marc.pinto@weareadaptive.com>
2026-01-09 06:01:35 -0500

    Fix consensus module snapshotting when there are 0 services so that leader's consensus module waits until uncommitted data is committed.

--- aeron-cluster/src/main/java/io/aeron/cluster/ConsensusModuleAgent.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/ConsensusModuleAgent.java
@@ -2610,7 +2610,7 @@ final class ConsensusModuleAgent
                     }
                     else
                     {
-                        terminateOnServiceAck(logAdapter.position());
+                        doTermination(logAdapter.position());
                     }
                 }
                 else
@@ -2671,10 +2671,6 @@ final class ConsensusModuleAgent
                         uncommittedState.offerLong(ConsensusModule.State.ACTIVE.code());
                         state(ConsensusModule.State.SNAPSHOT);
                         totalSnapshotDurationTracker.onSnapshotBegin(nowNs);
-                        if (0 == serviceCount)
-                        {
-                            snapshotOnServiceAck(logPublisher.position(), timestamp, ServiceAck.EMPTY_SERVICE_ACKS);
-                        }
                     }
                     break;
                 }
@@ -2696,7 +2692,7 @@ final class ConsensusModuleAgent
                     {
                         final long position = logPublisher.position();
 
-                        clusterTermination = new ClusterTermination(nowNs + ctx.terminationTimeoutNs(), serviceCount);
+                        clusterTermination = new ClusterTermination(nowNs + ctx.terminationTimeoutNs());
                         clusterTermination.terminationPosition(
                             ctx.countedErrorHandler(),
                             consensusPublisher,
@@ -2711,10 +2707,6 @@ final class ConsensusModuleAgent
                         uncommittedState.offerLong(ConsensusModule.State.ACTIVE.code());
                         state(ConsensusModule.State.SNAPSHOT);
                         totalSnapshotDurationTracker.onSnapshotBegin(nowNs);
-                        if (0 == serviceCount)
-                        {
-                            snapshotOnServiceAck(position, timestamp, ServiceAck.EMPTY_SERVICE_ACKS);
-                        }
                     }
                     break;
                 }
@@ -2723,7 +2715,7 @@ final class ConsensusModuleAgent
                 {
                     final CountedErrorHandler errorHandler = ctx.countedErrorHandler();
                     final long position = logPublisher.position();
-                    clusterTermination = new ClusterTermination(nowNs + ctx.terminationTimeoutNs(), serviceCount);
+                    clusterTermination = new ClusterTermination(nowNs + ctx.terminationTimeoutNs());
                     clusterTermination.terminationPosition(
                         errorHandler, consensusPublisher, activeMembers, thisMember, leadershipTermId, position);
                     terminationPosition = position;
@@ -2731,6 +2723,10 @@ final class ConsensusModuleAgent
                     if (serviceCount > 0)
                     {
                         serviceProxy.terminationPosition(terminationPosition, errorHandler);
+                    }
+                    else
+                    {
+                        clusterTermination.onTerminationReady();
                     }
                     state(ConsensusModule.State.TERMINATING);
                     break;
@@ -2741,6 +2737,14 @@ final class ConsensusModuleAgent
             }
 
             return 1;
+        }
+        else if (ConsensusModule.State.SNAPSHOT == state)
+        {
+            if (0 == serviceCount && logPublisher.position() <= commitPosition.getWeak())
+            {
+                final long timestamp = clusterClock.time();
+                snapshotOnServiceAck(commitPosition.getWeak(), timestamp, ServiceAck.EMPTY_SERVICE_ACKS);
+            }
         }
         else if (ConsensusModule.State.SUSPENDED == state)
         {
@@ -3623,6 +3627,10 @@ final class ConsensusModuleAgent
             {
                 serviceProxy.terminationPosition(terminationPosition, ctx.countedErrorHandler());
             }
+            else
+            {
+                clusterTermination.onTerminationReady();
+            }
             clusterTermination.deadlineNs(clusterClock.timeNanos() + ctx.terminationTimeoutNs());
             state(ConsensusModule.State.TERMINATING);
         }
@@ -3831,6 +3839,16 @@ final class ConsensusModuleAgent
 
     private void terminateOnServiceAck(final long logPosition)
     {
+        if (null != clusterTermination)
+        {
+            clusterTermination.onTerminationReady();
+        }
+
+        doTermination(logPosition);
+    }
+
+    private void doTermination(final long logPosition)
+    {
         if (null == clusterTermination)
         {
             if (terminationLeadershipTermId == leadershipTermId)
@@ -3848,7 +3866,6 @@ final class ConsensusModuleAgent
         }
         else
         {
-            clusterTermination.onServicesTerminated();
             if (clusterTermination.canTerminate(activeMembers, clusterClock.timeNanos()))
             {
                 recordingLog.commitLogPosition(leadershipTermId, logPosition);
