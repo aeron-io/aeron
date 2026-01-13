284a748e9a1fa550b2c696800107d4064bbbde96
marc-adaptive <marc.pinto@weareadaptive.com>
2026-01-12 21:15:53 -0500

    Tidy up on comments

--- aeron-system-tests/src/test/java/io/aeron/cluster/ClusterUncommittedStateTest.java
+++ aeron-system-tests/src/test/java/io/aeron/cluster/ClusterUncommittedStateTest.java
@@ -106,15 +106,14 @@ public class ClusterUncommittedStateTest
         cluster.suspendCluster(firstLeader);
         Tests.await(() ->
         {
-            boolean allNodesSuspended = true;
             for (int i = 0; i < cluster.memberCount(); ++i)
             {
                 if (ConsensusModule.State.SUSPENDED != cluster.node(i).moduleState())
                 {
-                    allNodesSuspended = false;
+                    return false;
                 }
             }
-            return allNodesSuspended;
+            return true;
         });
 
         IpTables.makeSymmetricNetworkPartition(CHAIN_NAME, leaderHostname, followerHostnames);
@@ -147,15 +146,14 @@ public class ClusterUncommittedStateTest
         cluster.suspendCluster(firstLeader);
         Tests.await(() ->
         {
-            boolean allNodesSuspended = true;
             for (int i = 0; i < cluster.memberCount(); ++i)
             {
                 if (ConsensusModule.State.SUSPENDED != cluster.node(i).moduleState())
                 {
-                    allNodesSuspended = false;
+                    return false;
                 }
             }
-            return allNodesSuspended;
+            return true;
         });
 
         IpTables.makeSymmetricNetworkPartition(CHAIN_NAME, leaderHostname, followerHostnames);
