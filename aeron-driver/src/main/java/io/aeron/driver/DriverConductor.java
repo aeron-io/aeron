2047d5b808dc691de1fc242a17ff44bd4430b806
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2026-02-26 10:00:59 +0100

    [Java] Store computed values for `isReliable` and `isSparse` instead of URI parameters in the log buffer metadata of the `PublicationImage`.
    (cherry picked from commit be468f6214d1ea7371a22437e795a064ac633150)

--- aeron-driver/src/main/java/io/aeron/driver/DriverConductor.java
+++ aeron-driver/src/main/java/io/aeron/driver/DriverConductor.java
@@ -396,13 +396,15 @@ public final class DriverConductor implements Agent
                 final boolean isMulticastSemantics =
                     isMulticastSemantics(subscriptionChannel, subscription.group(), flags);
                 final boolean isReliable = subscription.isReliable();
+                final boolean isSparse = isOldestSubscriptionSparse(subscriberPositions);
 
                 rawLog = newPublicationImageLog(
                     sessionId,
                     streamId,
                     initialTermId,
                     termBufferLength,
-                    isOldestSubscriptionSparse(subscriberPositions),
+                    isReliable,
+                    isSparse,
                     senderMtuLength,
                     channelEndpoint.socketRcvbufLength(),
                     channelEndpoint.socketSndbufLength(),
@@ -2213,6 +2215,7 @@ public final class DriverConductor implements Agent
         final int streamId,
         final int initialTermId,
         final int termBufferLength,
+        final boolean isReliable,
         final boolean isSparse,
         final int senderMtuLength,
         final int socketRcvBufLength,
@@ -2244,8 +2247,8 @@ public final class DriverConductor implements Agent
             params.receiverWindowLength,
             params.isTether,
             params.isRejoin,
-            params.isReliable,
-            params.isSparse,
+            isReliable,
+            isSparse,
             hasGroupSemantics,
             params.isResponse,
             publicationWindowLength,
