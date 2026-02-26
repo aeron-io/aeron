c6556024540aa1e55d8b168210b34c9a5c295314
marc-adaptive <marc.pinto@weareadaptive.com>
2026-02-26 11:06:32 -0500

    [Cluster] After SessionManager rebase

--- aeron-cluster/src/main/java/io/aeron/cluster/SessionManager.java
+++ aeron-cluster/src/main/java/io/aeron/cluster/SessionManager.java
@@ -36,6 +36,7 @@ import org.agrona.SemanticVersion;
 import org.agrona.Strings;
 import org.agrona.collections.ArrayListUtil;
 import org.agrona.collections.Long2ObjectHashMap;
+import org.agrona.collections.LongArrayQueue;
 import org.agrona.concurrent.CountedErrorHandler;
 import org.agrona.concurrent.errors.DistinctErrorLog;
 
@@ -74,6 +75,7 @@ class SessionManager
     private final ArrayList<ClusterSession> rejectedBackupSessions = new ArrayList<>();
 
     private final ArrayDeque<ClusterSession> uncommittedClosedSessions = new ArrayDeque<>();
+    private final LongArrayQueue uncommittedNextCommittedSessionIds = new LongArrayQueue(Long.MAX_VALUE);
 
     private final int memberId;
     private final ClusterClock clusterClock;
@@ -634,10 +636,10 @@ class SessionManager
                                 session.openedLogPosition(),
                                 nowNs,
                                 clusterTimeUnit);
-                            if (session.id() >= nextCommittedSessionId)
-                            {
-                                nextCommittedSessionId = session.id() + 1;
-                            }
+
+                            uncommittedNextCommittedSessionIds.offerLong(logPublisher.position());
+                            uncommittedNextCommittedSessionIds.offerLong(session.id() + 1);
+
                             ArrayListUtil.fastUnorderedRemove(pendingSessions, i, lastIndex--);
                             addSession(session);
                             workCount += 1;
@@ -1004,6 +1006,12 @@ class SessionManager
 
             uncommittedClosedSessions.pollFirst();
         }
+
+        while (uncommittedNextCommittedSessionIds.peekLong() <= commitPosition)
+        {
+            uncommittedNextCommittedSessionIds.pollLong();
+            nextCommittedSessionId = Math.max(nextCommittedSessionId, uncommittedNextCommittedSessionIds.pollLong());
+        }
     }
 
     void restoreUncommittedSessions(final long commitPosition)
@@ -1018,6 +1026,14 @@ class SessionManager
                 addSession(session);
             }
         }
+
+        while (uncommittedNextCommittedSessionIds.peekLong() <= commitPosition)
+        {
+            uncommittedNextCommittedSessionIds.pollLong();
+            nextCommittedSessionId = Math.max(nextCommittedSessionId, uncommittedNextCommittedSessionIds.pollLong());
+        }
+        uncommittedNextCommittedSessionIds.clear();
+        nextSessionId = nextCommittedSessionId;
     }
 
     void updateTimeOfLastActivity()
