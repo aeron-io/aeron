2dd50535a0a66b1915aeaaf8cec17815813b9919
marc-adaptive <marc.pinto@weareadaptive.com>
2026-04-17 14:29:27 -0400

    [C Driver] Invoke driver name resolver bootstrap resolver at relevant points

--- aeron-driver/src/main/c/aeron_driver_name_resolver.c
+++ aeron-driver/src/main/c/aeron_driver_name_resolver.c
@@ -159,6 +159,8 @@ static int aeron_driver_name_resolver_to_sockaddr(
 
 static bool aeron_driver_name_resolver_sockaddr_equals(struct sockaddr_storage *a, struct sockaddr_storage *b);
 
+static void aeron_name_resolver_log_and_clear_error(aeron_driver_name_resolver_t *resolver);
+
 static void aeron_driver_name_resolver_build_bootstrap_neighbor_counter_label(
     char *bootstrap_neighbor,
     struct sockaddr_storage *bootstrap_neighbor_address,
@@ -298,7 +300,9 @@ int aeron_driver_name_resolver_init(
         NULL != context->driver_name_resolver_bootstrap_resolver_supplier_func ?
         context->driver_name_resolver_bootstrap_resolver_supplier_func :
         aeron_default_name_resolver_supplier;
-    if (bootstrap_resolver_supplier_func(&_driver_resolver->bootstrap_resolver, NULL, context) < 0)
+    const char* bootstrap_resolver_init_args = context->driver_name_resolver_boostrap_resolver_init_args;
+    if (bootstrap_resolver_supplier_func(
+        &_driver_resolver->bootstrap_resolver, bootstrap_resolver_init_args, context) < 0)
     {
         AERON_APPEND_ERR("%s", "");
         goto error_cleanup;
@@ -521,6 +525,13 @@ error_cleanup:
 int aeron_driver_name_resolver_close(aeron_name_resolver_t *resolver)
 {
     aeron_driver_name_resolver_t *driver_resolver = (aeron_driver_name_resolver_t *)resolver->state;
+
+    if (driver_resolver->bootstrap_resolver.close_func(&driver_resolver->bootstrap_resolver) < 0)
+    {
+        AERON_APPEND_ERR("%s", "");
+        aeron_name_resolver_log_and_clear_error(driver_resolver);
+    }
+
     return aeron_driver_name_resolver_free(driver_resolver);
 }
 
@@ -1175,6 +1186,18 @@ int aeron_driver_name_resolver_set_resolution_header(
     return (int)entry_length;
 }
 
+int aeron_driver_name_resolver_lookup(
+    aeron_name_resolver_t *resolver,
+    const char *name,
+    const char *uri_param_name,
+    bool is_re_lookup,
+    const char **resolved_name)
+{
+    aeron_driver_name_resolver_t *driver_resolver = resolver->state;
+    return driver_resolver->bootstrap_resolver.lookup_func(
+        &driver_resolver->bootstrap_resolver, name, uri_param_name, is_re_lookup, resolved_name);
+}
+
 int aeron_driver_name_resolver_resolve(
     aeron_name_resolver_t *resolver,
     const char *name,
@@ -1216,6 +1239,13 @@ int aeron_driver_name_resolver_on_start(aeron_name_resolver_t *resolver)
 {
     aeron_driver_name_resolver_t *driver_resolver = (aeron_driver_name_resolver_t *)resolver->state;
     aeron_driver_name_resolver_resolve_bootstrap_neighbors(driver_resolver);
+
+    if (driver_resolver->bootstrap_resolver.start_func(&driver_resolver->bootstrap_resolver) < 0)
+    {
+        AERON_APPEND_ERR("%", "");
+        aeron_name_resolver_log_and_clear_error(driver_resolver);
+    }
+
     return 0;
 }
 
@@ -1253,6 +1283,8 @@ int aeron_driver_name_resolver_do_work(aeron_name_resolver_t *resolver, int64_t 
             driver_resolver->bootstrap_neighbor_resolve_deadline_ms = now_ms + driver_resolver->bootstrap_neighbor_resolution_interval_ms;
         }
 
+        work_count += driver_resolver->bootstrap_resolver.do_work_func(&driver_resolver->bootstrap_resolver, now_ms);
+
         driver_resolver->work_deadline_ms = now_ms + AERON_NAME_RESOLVER_DRIVER_DUTY_CYCLE_MS;
     }
 
@@ -1274,7 +1306,7 @@ int aeron_driver_name_resolver_supplier(
         return -1;
     }
 
-    resolver->lookup_func = aeron_default_name_resolver_lookup;
+    resolver->lookup_func = aeron_driver_name_resolver_lookup;
     resolver->resolve_func = aeron_driver_name_resolver_resolve;
     resolver->start_func = aeron_driver_name_resolver_on_start;
     resolver->do_work_func = aeron_driver_name_resolver_do_work;
