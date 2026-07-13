7a5a234a62a3294598899852ef58c3ecb1aa50ae
Michael Barker <mikeb01@gmail.com>
2026-07-14 10:11:51 +1200

    [Cluster] Small code cleanup of ClusterModuleLogger.

--- aeron-cluster/src/main/java/io/aeron/agent/ClusterModuleLogger.java
+++ aeron-cluster/src/main/java/io/aeron/agent/ClusterModuleLogger.java
@@ -14,28 +14,15 @@
  * limitations under the License.
  */
 package io.aeron.agent;
-/*
- * Copyright 2014-2025 Real Logic Limited.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import io.aeron.AeronCounters;
 import io.aeron.version.Versioned;
 import org.agrona.MutableDirectBuffer;
 import org.agrona.collections.Object2ObjectHashMap;
 
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Set;
 
 /**
  * Implementation of the {@link ModuleLogger} to handle logging and decode of Cluster log events.
@@ -46,27 +33,32 @@ public class ClusterModuleLogger implements ModuleLogger
     private static final Object2ObjectHashMap<String, EnumSet<ClusterEventCode>> SPECIAL_EVENTS =
         new Object2ObjectHashMap<>();
 
-    private static final EnumSet<ClusterEventCode> ENABLED_EVENT_CODES;
+    private static final Set<ClusterEventCode> ENABLED_EVENT_CODES;
 
     static
     {
         SPECIAL_EVENTS.put("all", EnumSet.allOf(ClusterEventCode.class));
+
         final String enabledEventCodes = System.getProperty("aeron.event.cluster.log");
         final String disabledEventCodes = System.getProperty("aeron.event.cluster.log.disable");
+
         final EnumSet<ClusterEventCode> disabledEventCodeSet = EventConfiguration.parseEventCodes(
             ClusterEventCode.class,
             disabledEventCodes,
             SPECIAL_EVENTS,
             ClusterEventCode::get,
             ClusterEventCode::valueOf);
-        ENABLED_EVENT_CODES = EventConfiguration.parseEventCodes(
+
+        final EnumSet<ClusterEventCode> enabledEventCodeSet = EventConfiguration.parseEventCodes(
             ClusterEventCode.class,
             enabledEventCodes,
             SPECIAL_EVENTS,
             ClusterEventCode::get,
             ClusterEventCode::valueOf);
 
-        ENABLED_EVENT_CODES.removeAll(disabledEventCodeSet);
+        enabledEventCodeSet.removeAll(disabledEventCodeSet);
+
+        ENABLED_EVENT_CODES = Collections.unmodifiableSet(enabledEventCodeSet);
     }
 
     /**
