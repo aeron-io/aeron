f1483c5346d2b86e9e7b1aa88e6deaeb782a401f
EmilJohn24 <emil.lopez@weareadaptive.com>
2026-07-14 15:30:56 +0800

    [Client] Moved reader-related object instantiation to EventReaderManager from EventConfiguration

---
+++ aeron-client/src/main/java/io/aeron/agent/EventReaderManager.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2014-2025 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.aeron.agent;
+
+import org.agrona.CloseHelper;
+import org.agrona.concurrent.Agent;
+import org.agrona.concurrent.AgentRunner;
+import org.agrona.concurrent.SleepingMillisIdleStrategy;
+import org.agrona.concurrent.UnsafeBuffer;
+import org.agrona.concurrent.ringbuffer.ManyToOneRingBuffer;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.ServiceLoader;
+
+import static io.aeron.agent.EventConfiguration.BUFFER_LENGTH_DEFAULT;
+import static io.aeron.agent.EventConfiguration.BUFFER_LENGTH_PROP_NAME;
+import static org.agrona.BitUtil.CACHE_LINE_LENGTH;
+import static org.agrona.BufferUtil.allocateDirectAligned;
+import static org.agrona.SystemUtil.getSizeAsInt;
+import static org.agrona.concurrent.ringbuffer.RingBufferDescriptor.TRAILER_LENGTH;
+
+/**
+ * Manages the lifecycle of the reader agent and its associated runner.
+ */
+final class EventReaderManager
+{
+    private static final String READER_CLASSNAME = "aeron.event.log.reader.classname";
+    private static final String READER_CLASSNAME_DEFAULT
+        = "io.aeron.agent.ModuleLoggerReaderAgent";
+
+    private final ManyToOneRingBuffer ringBuffer;
+    private Thread readerThread;
+    private AgentRunner readerAgentRunner;
+
+    EventReaderManager()
+    {
+        ringBuffer = new ManyToOneRingBuffer(new UnsafeBuffer(allocateDirectAligned(
+            getSizeAsInt(BUFFER_LENGTH_PROP_NAME, BUFFER_LENGTH_DEFAULT) + TRAILER_LENGTH, CACHE_LINE_LENGTH)));
+    }
+
+    ManyToOneRingBuffer ringBuffer()
+    {
+        return ringBuffer;
+    }
+
+    /**
+     * @param properties Restart the reader with the new properties.
+     */
+    void restart(final Properties properties)
+    {
+        readerThread = null;
+        CloseHelper.close(readerAgentRunner);
+        ringBuffer.unblock();
+        start(properties);
+    }
+
+    /**
+     * Start the reader agent and the buffer. Should only be called once
+     *
+     * @param properties to start the reader with.
+     */
+    public void start(final Properties properties)
+    {
+        final ArrayList<ModuleLogger> loggers = new ArrayList<>();
+        try
+        {
+
+            for (final ModuleLogger componentLogger : ServiceLoader.load(ModuleLogger.class))
+            {
+                loggers.add(componentLogger);
+            }
+
+
+            final Agent moduleLoggerReaderAgent = newReaderAgent(properties, loggers);
+
+            readerAgentRunner = new AgentRunner(
+                new SleepingMillisIdleStrategy(1L),
+                Throwable::printStackTrace,
+                null,
+                moduleLoggerReaderAgent);
+
+            readerThread = new Thread(readerAgentRunner);
+            readerThread.setName("event-log-reader");
+            readerThread.setDaemon(true);
+            readerThread.start();
+        }
+        catch (final Exception ex)
+        {
+            ex.printStackTrace(System.err);
+        }
+    }
+
+    private Agent newReaderAgent(final Properties configOptions, final List<ModuleLogger> loggers)
+    {
+        try
+        {
+            final Class<?> aClass = Class.forName(
+                configOptions.getProperty(READER_CLASSNAME, READER_CLASSNAME_DEFAULT));
+
+            try
+            {
+                final Constructor<?> constructor = aClass.getDeclaredConstructor(Properties.class, List.class);
+                return (Agent)constructor.newInstance(configOptions, loggers);
+            }
+            catch (final NoSuchMethodException ignore)
+            {
+            }
+
+            return (Agent)aClass.getDeclaredConstructor().newInstance();
+        }
+        catch (final Exception ex)
+        {
+            throw new RuntimeException(ex);
+        }
+    }
+}
