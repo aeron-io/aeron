dabca9e296b9c6583d4b69be940e324eb0bdf167
EmilJohn24 <emil.lopez@weareadaptive.com>
2026-08-20 15:56:40 +0800

    [Java] Initial implement of initial cpuset v2 reader.

---
+++ aeron-client/src/main/java/io/aeron/topology/AffinityParser.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2014-2026 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aeron.topology;
+
+import org.agrona.collections.IntArrayList;
+
+public class AffinityParser
+{
+    public static IntArrayList parse(final String affinity)
+    {
+        final IntArrayList result = new IntArrayList();
+        for (String part : affinity.split(","))
+        {
+            part = part.trim();
+            if (part.contains("-"))
+            {
+                final String[] range = part.split("-");
+                final int start = Integer.parseInt(range[0]);
+                final int end = Integer.parseInt(range[1]);
+                for (int cpu = start; cpu < end + 1; cpu++)
+                {
+                    result.add(cpu);
+                }
+            }
+            else
+            {
+                result.add(Integer.parseInt(part));
+            }
+        }
+        return result;
+    }
+}
---
+++ aeron-client/src/main/java/io/aeron/topology/CpusetV2Reader.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2014-2026 Real Logic Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aeron.topology;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static java.nio.channels.FileChannel.open;
+
+
+public class CpusetV2Reader
+{
+    private final Path procRoot;
+    private final Path cgroupRoot;
+
+    public CpusetV2Reader()
+    {
+        // TODO: Move to constants
+        this(Path.of("/proc"), Path.of("/sys/fs/cgroup"));
+    }
+
+    CpusetV2Reader(final Path procRoot, final Path cgroupRoot)
+    {
+        this.procRoot = procRoot;
+        this.cgroupRoot = cgroupRoot;
+    }
+
+    private Path retrieveEffectiveCgroupFilePath(final int pid) throws IOException
+    {
+        final Path procCgroupFilePath = procRoot.resolve(pid + "/cgroup");
+        try (Stream<String> lines = Files.lines(procCgroupFilePath))
+        {
+            final String effectiveCgroupPathStr = lines.filter(
+                line -> line.startsWith("0::/")).findFirst().orElseThrow();
+            return cgroupRoot.resolve(effectiveCgroupPathStr.substring(4));
+        }
+    }
+
+    public Cpuset readCpuSet(final int pid)
+    {
+        try
+        {
+            final Path effectiveCgroupFilePath = retrieveEffectiveCgroupFilePath(pid);
+            final String cpuGroups = Files.readString(effectiveCgroupFilePath);
+            return new Cpuset(AffinityParser.parse(cpuGroups));
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
